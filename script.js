0c61da95ff60f7ba58d2ba45ed3db908f15c9c16
oceanepistol14 <pistol.oceane.14@icloud.com>
2026-04-19 15:19:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const phrase= "Passionnée par le digital et la tech, je souhaite rejoindre L'Oréal pour contribuer à des projets innovants dès ma premières année de BTS SIO."
+function ecrireLettre(element, texte, callback) {
+    let i = 0;
+    element.style.opacity = 1;
+    const interval = setInterval(()=>{
+        element.textContent += texte[i];
+        i++;
+        if (i>= texte.length) {
+          clearInterval(interval);
+          if(callback) callback();
+
+        }
+    }, 40);
+ }
+
+ function afficher(element, callback){
+    element.classList.remove('cache');
+    setTimeout(callback, 800);
+ }
+
+ const logo = document.querySelector('.header');
+ const phraseEl = document.querySelector('.phrase-entreprise');
+ const carte = document.querySelector('.carte');
+ const bouton = document.querySelector(';droite a');
+
+ // 1. Logo
+ afficher(logo, () => {
+    // 3. Carte
+    afficher(carte, () => {
+        //4. boutons un par un 
+        boutons.forEach((btn, i)=>{
+            setTimeout(() => {
+              btn.classList.remove('cache');
+            }, i * 300);
+         })
+      })
+   })
+})
